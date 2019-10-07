bbc2bf5c5ae3a476d670241f79655a5c9f8a006c
Henrique <henrique.1360@gmail.com>
2019-10-07 09:46:20 -0300

    Initial commit

---
+++ src/routes/Route.js
@@ -0,0 +1,34 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable react/no-typos */
+/* eslint-disable react/jsx-props-no-spreading */
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Route, Redirect } from 'react-router-dom';
+
+export default function RouteWrapper({
+  component: Component,
+  isPrivate,
+  ...rest
+}) {
+  const signed = false;
+
+  if (!signed && isPrivate) {
+    return <Redirect to="/" />;
+  }
+
+  if (signed && !isPrivate) {
+    return <Redirect to="/dashboard" />;
+  }
+
+  return <Route {...rest} component={Component} />;
+}
+
+RouteWrapper.PropTypes = {
+  isPrivate: PropTypes.bool,
+  component: PropTypes.oneOfType([PropTypes.element, PropTypes.func])
+    .isRequired,
+};
+
+RouteWrapper.defaultProps = {
+  isPrivate: false,
+};
