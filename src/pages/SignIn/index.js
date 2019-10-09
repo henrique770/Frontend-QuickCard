84fc395e5a88973de93f8ca0d0eac2479f6b7432
Henrique <henrique.1360@gmail.com>
2019-10-09 10:09:21 -0300

    feat: Unform / Reactotron / Yup schema validation

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,7 +1,34 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform';
+import * as Yup from 'yup';
 
-// import { Container } from './styles';
+import logo from '~/assets/logo_quickcard.svg';
+
+const schema = Yup.object().shape({
+  email: Yup.string()
+    .email('Insira um email válido')
+    .required('O e-mail é obrigatório'),
+  password: Yup.string().required('A senha é obrigatória'),
+});
 
 export default function SignIn() {
-  return <h1>Login</h1>;
+  function handleSubmit(data) {
+    console.tron.log(data);
+  }
+
+  return (
+    <>
+      <div>
+        <img src={logo} alt="QuickCard" />
+
+        <Form schema={schema} onSubmit={handleSubmit}>
+          <Input name="email" type="email" placeholder="Seu e-mail" />
+          <Input name="password" type="password" placeholder="Sua senha" />
+          <button type="submit">Acessar</button>
+          <Link to="/cadastro">Criar conta gratuita</Link>
+        </Form>
+      </div>
+    </>
+  );
 }
--- src/pages/_layouts/auth/styles.js
+++ src/pages/_layouts/auth/styles.js
@@ -3,4 +3,74 @@ import styled from 'styled-components';
 export const Wrapper = styled.div`
   height: 100%;
   background: linear-gradient(-90deg, #f93b10, #fe650e);
+  display: flex;
+
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Content = styled.div`
+  width: 100%;
+  max-width: 350px;
+  background: #fff;
+  border-radius: 7px;
+  padding: 20px;
+  text-align: center;
+
+  form {
+    display: flex;
+    flex-direction: column;
+    margin-top: 30px;
+  }
+
+  input {
+    /* background: rgba(0, 0, 0, 0.1); */
+    border: solid 1px #e1e1e1;
+    border-radius: 4px;
+    height: 44px;
+    padding: 0 15px;
+    color: #111;
+    margin: 0 0 10px;
+
+    &::placeholder {
+      color: #fd570e;
+    }
+  }
+
+  span {
+    color: #f93b10;
+    align-self: flex-start;
+    margin: 0 0 10px;
+    font-weight: bold;
+  }
+
+  button {
+    margin: 5px 0 0;
+    height: 44px;
+    background: linear-gradient(-90deg, #fe650e, #f93b10);
+    font-weight: bold;
+    color: #fff;
+    border: 0;
+    border-radius: 4px;
+    font-size: 16px;
+    transition: background 0.2s;
+
+    &:hover {
+      background: linear-gradient(-90deg, #f93b10, #fe650e);
+    }
+  }
+
+  a {
+    color: #fd570e;
+    margin-top: 15px;
+    font-size: 16px;
+    opacity: 0.8;
+
+    &:hover {
+      opacity: 1;
+    }
+  }
+`;
+export const Container = styled.div`
+  background: #fff;
 `;
