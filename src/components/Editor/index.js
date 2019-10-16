24b895257b6f515cfc90006905c78fae1c56c74d
Henrique <henrique.1360@gmail.com>
2019-10-16 13:37:24 -0300

    feat: add editor component

--- src/components/Editor/index.js
+++ src/components/Editor/index.js
@@ -4,9 +4,9 @@ import * as ReactQuill from 'react-quill';
 import { Container } from './styles';
 
 class MyComponent extends React.Component {
-    constructor(props) {
+    constructor(props) {   
       super(props)
-      this.state = { text: "Mussum Ipsum, cacilds vidis litro abertis. Detraxit consequat et quo num tendi nada. Diuretics paradis num copo Mussum Ipsum, cacilds vidis litro abertis. Casamentiss faiz malandris se pirulitá. Mais vale um bebadis conhecidiss, que um alcoolatra anonimis. Si num tem leite então bota uma pinga aí cumpadi! Admodum accumsan disputationi eu sit. Vide electram sadipscing et per Sapien in monti palavris qui num significa nadis i pareci latim.  Quem manda na minha terra sou euzis! Interagi no mé, cursus quis, vehicula ac nisi. Viva Forevis aptent taciti sociosqu ad litora torquent. é motivis de denguis. Viva Forevis aptent taciti sociosqu ad litora torquent. Quem num gosta di mé, boa gentis num é. " } // You can also pass a Quill Delta here
+      this.state = { text: "Mussum Ipsum, cacilds vidis litro abertis. Detraxit consequat et quo num tendi nada. Diuretics paradis num copo Mussum Ipsum, cacilds vidis litro abertis. Casamentiss faiz malandris se pirulitá. Mais vale um bebadis conhecidiss, que um alcoolatra anonimis. num tem leite então bota uma pinga aí cumpadi! Admodum accumsan disputationi eu sit. Vide electram sadipscing et per Sapien in monti palavris qui num significa nadis i pareci latim.  Quem manda na minha terra sou euzis! Interagi no mé, cursus quis, vehicula ac nisi. Viva Forevis aptent taciti sociosqu ad litora torquent. é motivis de denguis. Viva Forevis aptent taciti sociosqu ad litora torquent. Quem num gosta di mé, boa gentis num é. " } // You can also pass a Quill Delta here
       this.handleChange = this.handleChange.bind(this)
     }
   
--- src/components/Editor/styles.js
+++ src/components/Editor/styles.js
@@ -3,5 +3,6 @@ import styled from 'styled-components';
 export const Container = styled.div`
   .ql-container {
       height: 90vh!important;
+      font-size: 16px
   }
 `;
