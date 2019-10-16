c491e0203a0e2f57456decd2f411b047b238c99c
Henrique <henrique.1360@gmail.com>
2019-10-16 13:33:20 -0300

    feat: feat: add editor component

---
+++ src/components/Editor/index.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import * as ReactQuill from 'react-quill';
+
+import { Container } from './styles';
+
+class MyComponent extends React.Component {
+    constructor(props) {
+      super(props)
+      this.state = { text: "Mussum Ipsum, cacilds vidis litro abertis. Detraxit consequat et quo num tendi nada. Diuretics paradis num copo Mussum Ipsum, cacilds vidis litro abertis. Casamentiss faiz malandris se pirulitá. Mais vale um bebadis conhecidiss, que um alcoolatra anonimis. Si num tem leite então bota uma pinga aí cumpadi! Admodum accumsan disputationi eu sit. Vide electram sadipscing et per Sapien in monti palavris qui num significa nadis i pareci latim.  Quem manda na minha terra sou euzis! Interagi no mé, cursus quis, vehicula ac nisi. Viva Forevis aptent taciti sociosqu ad litora torquent. é motivis de denguis. Viva Forevis aptent taciti sociosqu ad litora torquent. Quem num gosta di mé, boa gentis num é. " } // You can also pass a Quill Delta here
+      this.handleChange = this.handleChange.bind(this)
+    }
+  
+    handleChange(value) {
+      this.setState({ text: value })
+    }
+  
+    render() {
+      return (
+        <Container>
+        <div className="editor_style">
+        <ReactQuill theme="snow" value={this.state.text}
+                    onChange={this.handleChange} />
+        </div>                    
+        </Container>
+      )
+    }
+  }
+
+  export default MyComponent;
---
+++ src/components/Editor/styles.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  .ql-container {
+      height: 90vh!important;
+  }
+`;
