a54d8d86b9271a8d087dac651dcd88ac13c99717
Henrique <henrique.1360@gmail.com>
2019-10-17 13:44:47 -0300

    feat: page add note

---
+++ src/pages/AddNote/index.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import SideBar from '~/components/Sidebar';
+import Editor from '~/components/Editor';
+import { Container } from './styles';
+
+export default function AddNote() {
+
+  return (
+    <>
+      <Container>
+        <div className="">
+          <SideBar />
+        </div>
+        <div className="topbar">
+          <div className="containerside">
+            TÍTULO
+          </div>
+        </div>
+        <div className=" containerside">
+          
+          <div className="row mt-4 mr-4">
+          
+            <div className="col-md-12">
+              <Editor />
+            </div>
+          </div>
+          
+        </div>
+      </Container>
+    </>
+  );
+}
