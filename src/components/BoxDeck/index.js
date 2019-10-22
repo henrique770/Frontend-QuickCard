0f254879535b5dd84cd5917fec786fd13bce3c0f
Henrique <henrique.1360@gmail.com>
2019-10-22 13:05:54 -0300

    feat: add BoxDeck component

---
+++ src/components/BoxDeck/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Jumbotron } from 'react-bootstrap';
+import { Container } from './styles';
+
+export default function BoxDeck({ decks }) {
+  return (
+    <>
+      {decks.map(item => (
+        <Container>
+          <div className="item grow1">
+            <Jumbotron className="Jumbotron_height">
+              <h3>{item.titulo}</h3>
+              <div className="alignbottom linkbutton">
+                <Link to="/flashcard"><div className="Button">Acessar</div></Link>
+              </div>
+            </Jumbotron>
+          </div>
+        </Container>
+      ))}
+    </>
+  );
+}
