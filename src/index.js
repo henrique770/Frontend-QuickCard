d49c3d8471bf65b9a7250959f03b60aa96ca24f6
Henrique <henrique.1360@gmail.com>
2019-10-24 10:27:09 -0300

    test: add firebase database

--- src/index.js
+++ src/index.js
@@ -2,4 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const firebase = require('firebase');
+require('firebase/firestore');
+
+firebase.initializeApp({
+  apiKey: 'AIzaSyBVkZZ0rNSQ1-jJsHB51SDhw4r2_Eqq8UY',
+  authDomain: 'quick-card-7384d.firebaseapp.com',
+  databaseURL: 'https://quick-card-7384d.firebaseio.com',
+  projectId: 'quick-card-7384d',
+  storageBucket: 'quick-card-7384d.appspot.com',
+  messagingSenderId: '994385857118',
+  appId: '1:994385857118:web:a028677a9a0fc6449bc2cf',
+  measurementId: 'G-GBHFZLCTNJ',
+});
+firebase.analytics();
+
+ReactDOM.render(<App />, document.getElementById('quickcard-container'));
