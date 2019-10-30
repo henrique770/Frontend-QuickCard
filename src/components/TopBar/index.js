5cf37a5d9a2c187cc1a485d1eb6cf14b6e55296c
Henrique <henrique.1360@gmail.com>
2019-10-30 03:21:59 -0300

    feat: add TopBard component

---
+++ src/components/TopBar/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+export default function TopBar() {
+  return (
+    <Container>
+      <div className="topbar">
+        <div className="containerside">
+          <div className="hidden">t</div>
+        </div>
+      </div>
+    </Container>
+  );
+}
