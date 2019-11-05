6d70227b368fbb789bf5647c40452fafe3ee2a6a
Henrique <henrique.1360@gmail.com>
2019-11-04 23:39:41 -0200

    chore: api profile page

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,9 +1,76 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Form, Button } from 'react-bootstrap';
+import { useParams } from 'react-router-dom';
 import SideBar from '~/components/Sidebar';
 import { Container } from './styles';
+import ServiceApi from '../../services/ApiService/api.service';
 
 export default function Profile() {
+  const urlParams = useParams();
+  const [nomeCompleto, setNomeCompleto] = useState({});
+  const [email, setEmail] = useState({});
+  const [senhaAtual, setSenhaAtual] = useState({});
+  const [novaSenha, setNovaSenha] = useState({});
+  const [confirmarSenha, setConfirmarSenha] = useState({});
+
+  const UpdateDataProfile = async () => {
+    console.log(urlParams);
+    return ServiceApi.getCartaoById();
+  };
+
+  useEffect(() => {
+    const updateProfile = async () => {
+      const change = await UpdateDataProfile();
+
+      setNomeCompleto(change.nomeCompleto);
+      setEmail(change.email);
+      setSenhaAtual(change.senhaAtual);
+      setNovaSenha(change.NovaSenha);
+      setConfirmarSenha(change.confirmarSenha);
+    };
+
+    updateProfile();
+  }, []);
+
+  async function handleSubmit(data) {
+    data.preventDefault();
+    const changeBase = await UpdateDataProfile();
+
+    changeBase.nomeCompleto = nomeCompleto;
+    changeBase.email = email;
+    changeBase.senhaAtual = senhaAtual;
+    changeBase.novaSenha = novaSenha;
+    changeBase.confirmarSenha = confirmarSenha;
+
+    console.log(changeBase);
+
+    ServiceApi.updateCartao(urlParams.idBloco, changeBase);
+    alert('Perfil atualizado com sucesso!');
+  }
+
+  const handleChangeNomeCompleto = e => {
+    setNomeCompleto(e.target.value);
+  };
+
+  const handleChangeEmail = e => {
+    setEmail(e.target.value);
+  };
+
+  const handleChangeSenhaAtual = e => {
+    setSenhaAtual(e.target.value);
+  };
+  const handleChangeNovaSenha = e => {
+    if (senhaAtual !== novaSenha) {
+      alert('A senha não é correspondente');
+    } else {
+      setNovaSenha(e.target.value);
+    }
+  };
+
+  const handleChangeConfirmarSenha = e => {
+    setConfirmarSenha(e.target.value);
+  };
+
   return (
     <>
       <Container>
@@ -17,26 +84,46 @@ export default function Profile() {
           </div>
           <div className="row mt-4 mr-4">
             <div className="format_form">
-              <Form>
+              <Form onSubmit={handleSubmit}>
                 <div className="text-left labeltext">Nome Completo</div>
                 <Form.Group>
-                  <Form.Control type="text" placeholder="" />
+                  <Form.Control
+                    type="text"
+                    value={nomeCompleto}
+                    onChange={handleChangeNomeCompleto}
+                  />
                 </Form.Group>
                 <div className="text-left labeltext">Email</div>
                 <Form.Group>
-                  <Form.Control type="email" placeholder="" />
+                  <Form.Control
+                    type="email"
+                    value={email}
+                    onChange={handleChangeEmail}
+                  />
                 </Form.Group>
                 <div className="text-left labeltext">Senha atual</div>
                 <Form.Group>
-                  <Form.Control type="password" placeholder="" />
+                  <Form.Control
+                    type="password"
+                    value={senhaAtual}
+                    onChange={handleChangeSenhaAtual}
+                  />
                 </Form.Group>
                 <div className="text-left labeltext">Nova senha</div>
                 <Form.Group>
-                  <Form.Control type="password" placeholder="" />
+                  <Form.Control
+                    type="password"
+                    value={novaSenha}
+                    onChange={handleChangeNovaSenha}
+                  />
                 </Form.Group>
                 <Form.Group>
                   <div className="text-left labeltext">Confirmar senha</div>
-                  <Form.Control type="password" placeholder="" />
+                  <Form.Control
+                    type="password"
+                    value={confirmarSenha}
+                    onChange={handleChangeConfirmarSenha}
+                  />
                 </Form.Group>
 
                 <Button
